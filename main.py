633ae0b385da08b30fb6ce1521b280af857159e5
Jack Wiegman <wiegmanja@s.dcsdk12.org>
2017-12-18 08:27:25 -0700

    Fishish it

--- main.py
+++ main.py
@@ -1,78 +1,75 @@
 def class_amount():
 	
-		class_amount = raw_input("How many classes do you have? > ")
-		class_amount_int = int(class_amount)
+	class_amount = raw_input("How many classes do you have? > ")
+	class_amount_int = int(class_amount)
 
 	return class_amount_int
 
-def class_names():
-	classes_num = class_amount()
+def class_names_def():
 	class_names = []
-	for i in range(classes_num):
-		class_name = raw_input("What class is it? > ")
-		class_names.append(class_name)
+	class_name = raw_input("What class is it? > ")
 
-	return class_names
 
-def current_grades():
-	classes_num = class_amount()
+	return class_name
+
+def current_grades_def():
 	current_grades = []
 	current_grade = raw_input("What grade do you have in the class? > ")
-	for i in range(classes_num):
-		if "." not in current_grade:
-			current_grade = current_grade + ".0"
-		current_grades.append((float(current_grade)) / 100)
 
-	return current_grades
+	return current_grade
 
-def required_grades():
-	classes_num = class_amount()
+def required_grades_def():
 	required_grades = []
 	required_grade = raw_input("What grade do you want to finish with? > ")
-	for i in range(classes_num):
-		if "." not in required_grade:
-			required_grade = required_grade + ".0"
-		required_grades.append((float(required_grade)) / 100)
 
-	return required_grades
+	return required_grade
 
-def final_worths():
-	classes_num = class_amount()
+def final_worths_def():
 	final_worths = []
 	final_worth = raw_input("How much is your final worth? > ")
-	for i in range(classes_num):
-		if "." not in final_worth:
-			final_worth = final_worth + ".0"
-		final_worths.append((float(final_worth)) / 100)
 
-	return final_worths
+	return final_worth
 
 def calculate():
 	final_grades_needed = []
 
-	# classes = class_names()
-	# grades = current_grades()
-	# grades_wanted = required_grades()
-	# final_weights = final_worths()
+	class_names = []
+	current_grades = []
+	required_grades = []
+	final_worths = []
 
-	# final_grade_needed = (grades_wanted[0] - ((1 - final_weights[0]) * (grades[0]))) / final_weights[0]
-	# final_grades_needed.append(final_grade_needed)
-
-	# print "In %s you need a %s percent" % (classes[0], str((final_grades_needed[0] * 100)))
-
-	classes = class_names()
-	grades = current_grades()
-	grades_wanted = required_grades()
-	final_weights = final_worths()
+	class_amount_int = class_amount()
 
 	for i in range(class_amount_int):
-		final_grade_needed = (grades_wanted[i] - ((1 - final_weights[i]) * (grades[i]))) / final_weights[i]
+		
+		classes = class_names_def()
+		class_names.append(classes)
+
+		grade = current_grades_def()
+		if "." not in grade:
+			grade = grade + ".0"
+		current_grades.append((float(grade)) / 100)
+
+		grade_wanted = required_grades_def()
+		if "." not in grade_wanted:
+			grade_wanted = grade_wanted + ".0"
+		required_grades.append((float(grade_wanted)) / 100)
+
+		final_weight = final_worths_def()
+		if "." not in final_weight:
+			final_weight = final_weight + ".0"
+		final_worths.append((float(final_weight)) / 100)
+
+
+		final_grade_needed = (required_grades[i] - ((1 - final_worths[i]) * (current_grades[i]))) / final_worths[i]
 		final_grades_needed.append(final_grade_needed)
 
-		print "In %s you need a %s percent" % (classes[i], str((final_grades_needed[i] * 100)))
+		print "In %s you need a %s percent\n" % (class_names[i], str((final_grades_needed[i] * 100)))
 
 	for i in range(class_amount_int):
-		print "\nIn %s you need a %s percent" % (classes[i], str((final_grades_needed[i] * 100)))
+		print "In %s you need a %s percent" % (class_names[i], str((final_grades_needed[i] * 100)))
+
+
 
 
 calculate()
